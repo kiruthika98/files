6e79a9990e30caac36700d415296f9fe077b0299
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-03-14 08:22:49 +0530

    middle

---
+++ middlenumber.py
@@ -0,0 +1,6 @@
+def findMiddle(input_list):
+    middle = float(len(input_list))/2
+    if middle % 2 != 0:
+        return input_list[int(middle - .5)]
+    else:
+        return (input_list[int(middle)], input_list[int(middle-1)])
