e2dd125778f981456e3ce36e0cc597fa8fee09bf
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-03-22 20:20:49 +0530

    number

---
+++ smallhighnumber.py
@@ -0,0 +1,7 @@
+lst = []
+n = int(input('How many numbers: '))
+for n in range(n):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("maximum element in the list is :", max(lst), "\nMinimum element in the list is :", min(lst))
+
