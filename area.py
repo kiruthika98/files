12abff0e95b636a55c8754bcc65ad244aecb7187
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-03-22 19:19:11 +0530

    area

---
+++ area.py
@@ -0,0 +1,7 @@
+length = float(' please enter the length :'))
+width =  float(' please enter the width :'))
+height =  float(' please enter the height :'))
+SA = 2 * (length*width+length*height+width*height)
+volume = length*width*height
+print("\n the surfacearea of acuboid=%.2f"%SA)
+print("the volume of the cuboid =%2f"%volume);
