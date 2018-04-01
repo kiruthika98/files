ec0beb2c6fcbfee6a4922d8acd24a410dea7a015
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-04-01 20:26:35 +0530

    10numbers

---
+++ minimumamong10num.py
@@ -0,0 +1,6 @@
+lst = []
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("Minimum element in the list is :", min(lst)
