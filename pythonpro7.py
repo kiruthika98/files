a41b52ede71eab8fb3b0415b8edb7e9bd48d34a7
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-04-01 21:26:38 +0530

    abc

---
+++ pythonpro7.py
@@ -0,0 +1,13 @@
+def matched(str):
+    ope = []
+    clo = []
+    for i in range(0,len(str)):
+        l = str[i]
+        if l == "(":
+            ope = ope + ["("]
+        elif l == ")":
+            clo = clo  + [")"]
+    if len(ope)==len(clo):
+        return True
+    else:
+        return Faluse
