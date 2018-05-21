ff9dabf5a9ca652496b547898b45a6a906c4a5ef
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-05-21 14:43:58 +0530

    new18

---
+++ finaloddoreven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+int k,l;
+int diff;
+scanf("%d %d",&k,&l);
+  if(b>k)
+  {
+    diff=l-k;
+  }else
+  {
+diff=k-l;
+  }
+if(diff%2==0)
+printf("even");
+else
+printf("odd");
+return 0;
+}
+
